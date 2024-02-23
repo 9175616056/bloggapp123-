7120e24a8c14723abf0d76596f1cb50757b0ed11
9175616056 <rohanawale73@gmail,com>
2024-02-23 14:24:44 +0530

    r

---
+++ src/main/java/com/blogapp11/Blogapp11Application.java
@@ -0,0 +1,13 @@
+package com.blogapp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogapp11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blogapp11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogapp11/Blogapp11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogapp11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blogapp11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
